066d2d42ab7cba06797187eb61b8de35c9d80954
valentin viger <vigervalentin@gmail.com>
2022-01-18 09:48:18 +0100

    Empty project

---
+++ hardhat.config.ts
@@ -0,0 +1,68 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import '@tenderly/hardhat-tenderly';
+
+require("dotenv").config();
+
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        }
+      }
+    ],
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/" + (process.env.ALCHEMY_API_KEY || '')
+      }
+    },
+    mainnet: {
+      url: process.env.MAINNET_URI,
+      accounts: [process.env.MAINNET_PRIVATE_KEY || ''],
+    },
+    tenderly: {
+      url: "https://rpc.tenderly.co/fork/" + (process.env.TENDERLY_FORK_ID || ''),
+      accounts: [process.env.MAINNET_PRIVATE_KEY || ''],
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY || ''
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5"
+  },
+  gasReporter: {
+    enabled: false
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT || '',
+    username: process.env.TENDERLY_USERNAME || '',
+    forkNetwork: '1', //Network id of the network we want to fork
+  }
+};
+
+export default config;
