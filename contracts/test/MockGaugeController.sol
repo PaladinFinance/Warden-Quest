a32f4426f99efa578e43baa7ee6a6a1cced45d51
valentin viger <vigervalentin@gmail.com>
2022-08-02 16:15:18 +0200

    Tests for Light Quest

--- contracts/test/MockGaugeController.sol
+++ contracts/test/MockGaugeController.sol
@@ -7,6 +7,14 @@ pragma solidity ^0.8.0;
  */
 contract MockGaugeController {
     
+    uint256 constant public MAXTIME = 4 * 365 * 86400;  // 4 years
+
+    struct VotedSlope {
+        uint slope;
+        uint power;
+        uint end;
+    }
+
     struct Point {
         uint bias;
         uint slope;
@@ -18,6 +26,12 @@ contract MockGaugeController {
     mapping(address => mapping(uint256 => Point)) point_weights;
 
     mapping(address => int128) types;
+
+    // gauge => user => vote_slope
+    mapping(address => mapping(address => VotedSlope)) userVotes;
+    // gauge => user => vote ts
+    mapping(address => mapping(address => uint256)) lastUserVote;
+
 
     function set_points_weight(address gauge, uint256 period, uint256 bias) external {
         point_weights[gauge][period].bias = bias;
@@ -40,6 +54,34 @@ contract MockGaugeController {
 
     function gauge_types(address gauge) external view returns(int128){
         return types[gauge] - 1;
+    }
+
+    function set_user_vote(
+        address user,
+        address gauge,
+        uint256 period,
+        uint256 amount,
+        uint256 end
+    ) external {
+        uint256 slope = amount / MAXTIME;
+        /*uint256 dt = end - block.timestamp;
+        uint256 bias = slope * dt;*/
+
+        userVotes[gauge][user] = VotedSlope(
+            slope,
+            10000, //We don't care about the percent of balance here, can put all to 100%, since we can put multiple votes per voters
+            end
+        );
+
+        lastUserVote[gauge][user] = period;
+    }
+
+    function vote_user_slopes(address user, address gauge) external view returns(VotedSlope memory){
+        return userVotes[gauge][user];
+    }
+
+    function last_user_vote(address user, address gauge) external view returns(uint){
+        return lastUserVote[gauge][user];
     }
     
 }
